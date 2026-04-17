b1e22875482f636fd90a0062308b6a3fa2a2140b
gokcesen <sengkce@gmail.com>
2026-04-17 20:55:28 +0200

    feat: add page footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Navbar from './components/Navbar';
 import RestaurantList from './components/RestaurantList';
 import { mockRestaurants } from './data/mockRestaurants';
+import Footer from './components/Footer';
 
 function App() {
   const [postcode, setPostcode] = useState('EC4M7RF');
@@ -10,15 +11,19 @@ function App() {
     console.log('Search postcode:', postcode);
   };
   return (
-    <main className="min-h-screen bg-slate-50 px-4 py-10 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-7xl">
+    <main className="flex min-h-screen flex-col bg-slate-50 px-4 py-10 sm:px-6 lg:px-8">
+      <div className="mx-auto flex w-full max-w-7xl flex-1 flex-col">
         <Navbar
           postcode={postcode}
           onPostcodeChange={setPostcode}
           onSubmit={handleSearch}
         />
 
-        <RestaurantList restaurants={mockRestaurants} />
+        <div className="flex-1">
+          <RestaurantList restaurants={mockRestaurants} />
+        </div>
+
+        <Footer />
       </div>
     </main>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="pt-10 text-center text-sm text-slate-500">
+      © 2026 Gokce. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
